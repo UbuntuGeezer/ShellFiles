c5bfeb0e01925e0df8dfa6b7ad3922d79397944c
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-17 18:29:20 -0400

    PJDUMP support - BU-Ignores.sh added

--- showgit.sh
+++ showgit.sh
@@ -1,4 +1,4 @@
-#git rev-list --format=%B --max-count=1 36ae370
+#git rev-list --format=%B --max-count=1 64d4f20
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is 36ae370; Press <return> to continue>..."
+read -p "Latest ShellFiles is 64d4f20; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
