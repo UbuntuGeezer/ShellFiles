3f784cdeac150ad95cefc82ab01095341d41e08b
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-18 20:02:07 -0400

    Installgit.sh modified to message SystemLog.txt

--- Installgit.sh
+++ Installgit.sh
@@ -1,8 +1,11 @@
 #Installgit.sh - install git: on transient Ubuntu.
-#	7/12/20.	wmk.	08:30
+#	7/18/20.	wmk.	18:00
+log_file=$HOME/SystemLog.txt
 sudo apt update
 git -version
 sudo apt install git
 echo "git installed from repository."
+date +%T >> $log_file
+echo "  git installed from repository." >> $log_file
 read -p "Press Enter to ontinue.."
 
