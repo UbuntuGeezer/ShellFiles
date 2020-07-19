7f84cc07a016bf9e0a0b07da1a59a64710247b21
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-19 09:00:45 -0400

    NewSSHKey added; StartupNotes updated

---
+++ NewSSHKey.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# NewSSHKey.sh - generate and register new SSHKey.
+#	7/19/20.	wmk.	07:45
+# Usage.  bash NewSSHKey.sh <.ssh filespec>
+#
+#	<.ssh filespec> = ~/.ssh/file_name where file_name = id_rsa, id_rsb, etc.
+# Method. checks to see if ssh-agent process runnng; if not start it
+#	use ssh-keygen to generate new key at <.ssh filespec>
+# 	use ssh -add <.ssh filespec> to add new key to agent
+#	remind user to add <filespec>.pub content to host registry
+#	issue SystemLog message "  new .ssh key generated and added - <.ssh filespec>
+date +%T >> $system_log #
+echo "  NewSSHKey initiated." >> $system_log #
+echo "" > $TEMP_PATH/scratch #
+#pgrep -l ssh-agent >> $TEMP_PATH/scratch #
+pgrep -l ssh-agent
+if [ $? -eq 0 ] ; then
+echo " ssh-agent running..."
+echo "Inserted echo to see if redirect comes back.."
+date +%T >> $system_log #
+echo "  NewSSHKey (test) complete." >> $system_log #else
+else
+  exec ssh-agent bash
+  date +%T >> $system_log #
+  echo "  ssh-agent started." >> $system_log #
+fi
+
--- showgit.sh
+++ showgit.sh
@@ -1,4 +1,4 @@
-#git rev-list --format=%B --max-count=1 64d4f20
+#git rev-list --format=%B --max-count=1 3f784cd
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is 64d4f20; Press <return> to continue>..."
+read -p "Latest ShellFiles is 3f784cd; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
