b45a7d74bab3805f8526965c30262bba40440fc6
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-19 14:55:12 -0400

    PJDUMP bug fixes - again

--- showgit.sh
+++ showgit.sh
@@ -1,4 +1,4 @@
-#git rev-list --format=%B --max-count=1 3f784cd
+#git rev-list --format=%B --max-count=1 38c4e5e
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is 3f784cd; Press <return> to continue>..."
+read -p "Latest ShellFiles is 38c4e5e; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
