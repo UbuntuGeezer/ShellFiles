a6446a7ba060101bb9281d26d466ad2899f933cd
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-25 06:24:16 -0400

    NewSSHKey added to .gitignore

--- showgit.sh
+++ showgit.sh
@@ -1,4 +1,4 @@
-#git rev-list --format=%B --max-count=1 3b66e81
+#git rev-list --format=%B --max-count=1 970da74
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is 3b66e81; Press <return> to continue>..."
+read -p "Latest ShellFiles is 970da74; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
