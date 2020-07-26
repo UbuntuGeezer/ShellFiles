93c6e4fac697643071576fb742f8596a0a10cc54
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-26 18:40:50 -0400

    SETMSG updated, ZoomIDs.sh added

---
+++ ZoomIDs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ZoomIDs.sh - display c:ommonly used Zoom IDs for reference.
+#	7/26/20.	wmk.	18:15
+# Usage.  bash ZoomIDs.sh
+#
+# Exit.  Zoom meeting IDs displayed in Terminal session.
+#
+echo "North Venice Public Meetings  248 257 731 / 2588"
+echo "Paul Nagle's Personal Meetings 850 252 6271 / 2000"
+echo "Field Service Meetings - M-F  425 981 905 / 2588"
+echo "Tom VanWieren's Personal Meetings 794 245 9910 / 884166"
+echo "Bill Kaercher's Personal Meetings 949 489 8834 / 1z2fUn"
+read -p "Press Enter to continue.."
--- showgit.sh
+++ showgit.sh
@@ -1,4 +1,4 @@
-#git rev-list --format=%B --max-count=1 970da74
+#git rev-list --format=%B --max-count=1 30e3e6c
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is 970da74; Press <return> to continue>..."
+read -p "Latest ShellFiles is 30e3e6c; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
