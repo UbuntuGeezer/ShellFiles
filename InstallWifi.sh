f6376b1bd46cefb0e8e4c988c5f57d18759340e1
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-07-27 11:56:48 -0400

    COLDSTART_1, WARMSTART, SYSEDIT, InstallWifi.sh corrections

--- InstallWifi.sh
+++ InstallWifi.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 # InstallWifi.sh - Install drivers for HP Pavilion wifi from repository.
-#	7/25/20.	wmk.	12:00
+#	7/27/20.	wmk.	07:15
 # Usage.  bash InstallWifi.sh
 # Entry.  F2 on system startup to ensure wifi card is "On"
 # (instructions from ubuntuhandbook.org)
 #
-# Notes. "no WiFi adaptor found (ubuntu 18.0.4
+# Notes. "no WiFi adaptor found (ubuntu 18.0.4)
 # This happens on many HP devices including HP 15-bs000, 15-bs100,
 # 15g-br000, 15q-bu000, and Pavilion 15-br000, HP 280 G3, 290 G1,
 # and more. And the reason is Ubuntu does not support RTL8723de
 # wifi driver. The following comes off the ubuntuhandbook.org site.
 #
+# check for mandatory "build" parameter before running
+date +%T >> $system_log #
+echo "  Installing wifi driver." >> $system_log #
+echo "  Installing wifi driver."
+if [ -z "$1" ]; then
+  echo "  'build' parameter missing"
+  echo "   No 'build' parameter.." >> $system_log #
+  echo "  wifi driver installation abandoned." >> $system_log #
+  echo "  wifi driver installation abandoned."
+  exit 0
+else
+  read -p "Install wifi will rebuild device driver.. continue (y/n)? "
+  if [ $REPLY == 'y' ] || [ $REPLY == 'Y' ]; then
+    echo "If not debugging, wifi driver would build here.."
+    echo "  exiting"
+    exit 0
+  else
+    echo "  wifi driver build cancelled by user." >> $system_log #
+    echo "  wifi driver build cancelled - exiting"
+    exit 0
+  fi     # end user ok to rebuild conditional
+fi # end empty command line conditional
+if [ true ]; then
 # install the building tools
 sudo apt-get install linux-headers-$(uname -r) build-essential git
 # get newest Realtek rtlwifi codes
@@ -53,3 +76,4 @@ sudo modprobe -r rtl8723de && sleep 5 && sudo modprobe rtl8723de ant_sel=2
 # where X is the antenna that worked best
 echo "options rtl8723de ant_sel=X" | sudo tee /etc/modprobe.d/rtl8723de.conf
 fi
+fi  # end [false] code skip
