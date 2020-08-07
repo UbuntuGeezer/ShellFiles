790b89245bf5bdbd8acf91c81acded61ff38deaf
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2020-08-06 23:00:23 -0400

    ENDSQL updated to allow user input end session message

--- showgit.sh
+++ showgit.sh
@@ -1,4 +1,4 @@
-##git rev-list --format=%B --max-count=1 e121966
+##git rev-list --format=%B --max-count=1 aa0098b
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is e121966; Press <return> to continue>..."
+read -p "Latest ShellFiles is aa0098b; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
