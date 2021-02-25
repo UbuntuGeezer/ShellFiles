ca54b20885ddb6a05702b717c5d5a030e7c61607
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2021-02-25 07:37:41 -0500

    Revert PJDUMP; PJDUMP_dev included

--- showgit.sh
+++ showgit.sh
@@ -1,5 +1,5 @@
-#git rev-list --format=%B --max-count=1 ccffbd6
+#git rev-list --format=%B --max-count=1 f424205
 10
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is ccffbd6; Press <return> to continue>..."
+read -p "Latest ShellFiles is f424205; Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
