f76c922bcd975c4256ec50effff524d4b65bf341
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2021-04-19 05:42:49 -0400

    COLDSTART COLDSTART_1 improvements

--- showgit.sh
+++ showgit.sh
@@ -1,5 +1,5 @@
-#git rev-list --format=%B --max-count=1 cd366a1
+#git rev-list --format=%B --max-count=1 a3f2ec9
 10
 echo "See commit-list.txt for list of commits..."
-read -p "Latest ShellFiles is cd366a1. Press <return> to continue>..."
+read -p "Latest ShellFiles is a3f2ec9. Press <return> to continue>..."
 git rev-list --format=%B --max-count=1 $1  # comment
