1e2e8bf230fd73cf30b8ea1f4bc004901e778807
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2021-06-15 23:28:32 -0500

    aliases.sh documented; persistent system check in BACKLOG

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,16 @@
 # aliases.sh - shell file to reveal standard git aliases
 # from githowto.com/aliases
-#	wmk.	5/9/20.
+#	wmk.	6/15/21.
+#
+#	Exit. ~/.gitconfig contains git command aliases, read by git startup.
+#
+# Notes. COLDTART depends upon .gitconfig residing in ubuntu/sysstart folder
+# then copies it to ~/ folder.
+#
+# Modification History.
+# ---------------------
+# 5/9/20.	wmk.	original code.
+# 6/15/21.	wmk.	documentation.
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
