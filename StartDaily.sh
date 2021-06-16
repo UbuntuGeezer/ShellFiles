560e6c909a177a91090b63bf338afd05f3129d24
UbuntuGeezer <63589184+UbuntuGeezer@users.noreply.github.com>
2021-06-16 00:14:40 -0500

    COLDSTART, COLDSTART_1 QUICKSTART, WARMSTART, StartDaily multihost support

--- StartDaily.sh
+++ StartDaily.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # StartDaily.sh - start daily timer task(s).
-#	5/14/21.	wmk.
-systemd-run --user --on-calendar 'daily' /bin/bash -c "date >> /media/ubuntu/Windows/Users/Bill/ubuntu/SystemLog.txt"
-~/sysprocs/LOGMSG "systemd-run --user --on-calendar 'daily' /bin/bash -c 'date >> /media/ubuntu/Windows/Users/Bill/ubuntu/SystemLog.txt'"
+#	6/15/21.	wmk.
+#
+# Modification History.
+# ---------------------
+# 5/14/21.	wmk.	original code.
+# 6/15/21.	wmk.	multihost support added.
+if [ "$HOME" == "/home/ubuntu" ]; then
+ folderbase=/media/ubuntu/Windows/Users/Bill
+else
+ folderbase=$HOME
+fi
+systemd-run --user --on-calendar 'daily' /bin/bash -c "date >> $folderbase/ubuntu/SystemLog.txt"
+~/sysprocs/LOGMSG "systemd-run --user --on-calendar 'daily' /bin/bash -c 'date >> $folderbase/ubuntu/SystemLog.txt'"
 
 
